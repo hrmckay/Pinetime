28393689ce9ff57b5a944105a2e08b830e1b271b
Hugh McKay <hrmckay@sympatico.ca>
2020-10-08 18:47:07 +0000

    Commit from GitHub Actions

--- docs/lvgl.js
+++ docs/lvgl.js
@@ -1152,9 +1152,9 @@ function updateGlobalBufferAndViews(buf) {
   Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
 }
 
-var STACK_BASE = 5730768,
+var STACK_BASE = 5730800,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 487888;
+    STACK_MAX = 487920;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 
